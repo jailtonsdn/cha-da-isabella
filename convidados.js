deaa57662dd1646182a97e1b4aa940d1f3605c4d
Jailton Nascimento <108580162+jailtonsdn@users.noreply.github.com>
2025-04-15 16:18:51 -0300

    Update convidados.js

--- convidados.js
+++ convidados.js
@@ -2,5 +2,6 @@ const convidados = {
   "isadora": { fralda: "M – Pampers" },
   "jailton": { fralda: "G – Huggies" },
   "izabel": { fralda: "P – Pampers" },
+  "maiza": { fralda: "P – Pampers" },
   // ... aqui vai a lista completa que eu vou gerar depois
 };
